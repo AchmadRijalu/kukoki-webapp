e75ae03f6b143b37831e36e4eda9ae7690108999
Maximus Aurelius Wiranata <maximusaureliusw@gmail.com>
2022-11-26 16:27:15 +0700

    finish home without card

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
     return (
         <nav className="bg-blue-bg bg-cover bg-no-repeat text-white flex items-center justify-between py-3 px-8">
             <div>
-                <img src="img/logo/logo.svg" alt="" className="w-28" />
+                <img src="img/logo/logo.svg" alt="" className="w-32" />
             </div>
-            <div className="flex gap-8 text-white font-bold">
+            <div className="flex gap-8 text-white font-bold text-lg">
                 <Link href="">Menu</Link>
                 <Link href="">Rencana</Link>
                 <Link href="">Pesanan</Link>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -37,9 +37,156 @@ export default function Welcome(props) {
 
                 <div className="bg-blue-bg bg-cover bg-no-repeat">
                     <HeaderNoBg />
+                    <div className="grid grid-cols-2 px-12 text-white">
+                        <div className="flex items-center">
+                            <div>
+                                <h1 className="text-7xl font-bold leading-tight">
+                                    Masak Tanpa <br /> Ribet!
+                                </h1>
+                                <h2 className="text-xl font-semibold mb-8">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Maxime, quibusdam!
+                                </h2>
+                                <Link
+                                    href=""
+                                    className="text-darkblue bg-white py-3 px-10 font-semibold rounded-md hover:bg-gray-200 transition"
+                                >
+                                    Pesan Sekarang
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <img
+                                src="img/home/jumbotron.svg"
+                                alt=""
+                                className="w-5/6"
+                            />
+                        </div>
+                    </div>
+                    <div className="h-1 bg-brown"></div>
                 </div>
 
-                <div className="h-96"></div>
+                <div className="flex justify-center">
+                    <div className="flex items-center flex-col">
+                        <h1 className="text-center font-bold text-5xl text-blue my-5">
+                            Temukan Meal Kit Favoritmu
+                        </h1>
+                        <h2 className="text-center font-semibold text-xl text-darkblue mb-5">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Non, quis?
+                        </h2>
+                        <Link
+                            href=""
+                            className="text-white my-5 bg-blue py-3 px-10 font-semibold rounded-md hover:bg-darkblue transition"
+                        >
+                            Lihat Semua
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="bg-blue-bg bg-cover bg-no-repeat flex justify-center text-white py-5">
+                    <div>
+                        <h1 className="text-center font-bold text-5xl my-5">
+                            Bagaimana Cara Pesan
+                        </h1>
+                        <div className="md:grid grid-cols-3 gap-20 mt-5 md:px-0">
+                            <div className="flex items-center flex-col p-5">
+                                <img
+                                    src="img/home/pilih_mealkit.svg"
+                                    className="h-32"
+                                    alt=""
+                                />
+                                <h2 className="text-center my-3 text-2xl font-semibold">
+                                    Pilih Meal Kit
+                                </h2>
+                                <p className="text-center mb-3 px-10">
+                                    Kukoki menyediakan banyak variasi meal kit
+                                    yang siap untuk disajikan. Pilih meal kit
+                                    sesuai keinginan anda
+                                </p>
+                            </div>
+                            <div className="flex items-center flex-col p-5">
+                                <img
+                                    src="img/home/pesan.svg"
+                                    className="h-32"
+                                    alt=""
+                                />
+                                <h2 className="text-center my-3 text-2xl font-semibold">
+                                    Pesan
+                                </h2>
+                                <p className="text-center mb-3 px-10">
+                                    Pesan meal kit sesuai keinginan. Kukoki akan
+                                    menyiapkan meal kit pesanan anda
+                                </p>
+                            </div>
+                            <div className="flex items-center flex-col p-5">
+                                <img
+                                    src="img/home/pengiriman.svg"
+                                    className="h-32"
+                                    alt=""
+                                />
+                                <h2 className="text-center my-3 text-2xl font-semibold">
+                                    Pengiriman
+                                </h2>
+                                <p className="text-center mb-3 px-10">
+                                    Setelah melakukan pemesanan, meal kit akan
+                                    dikirimkan pada tanggal yang telah dipilih.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                    <div className="bg-darkblue">
+                        <div className="flex justify-center">
+                            <img src="img/home/gratis_ongkir.svg" alt="" />
+                        </div>
+                    </div>
+                    <div className="px-10 flex items-center">
+                        <div>
+                            <h1 className="font-bold text-5xl text-blue">
+                                Gratis Ongkir
+                            </h1>
+                            <p className="text-xl mb-5">
+                                Dapatkan gratis ongkir untuk transaksi pertama
+                                anda.
+                            </p>
+                            <Link
+                                href=""
+                                className="text-white bg-blue py-3 px-10 font-semibold rounded-md hover:bg-darkblue transition"
+                            >
+                                Pesan Sekarang
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                    <div className="px-10 flex items-center">
+                        <div>
+                            <h1 className="font-bold text-5xl text-blue">
+                                Meal Kit Sesuai <br />
+                                Preferensi Anda
+                            </h1>
+                            <p className="text-xl mb-5">
+                                Pilih preferensi meal kit untuk mendapatkan
+                                rekomendasi sesuai keinginan anda.
+                            </p>
+                            <Link
+                                href=""
+                                className="text-white bg-blue py-3 px-10 font-semibold rounded-md hover:bg-darkblue transition"
+                            >
+                                Atur Preferensi
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="bg-darkblue">
+                        <div className="flex justify-center">
+                            <img src="img/home/mealkit_preferensi.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
 
                 <Footer />
             </div>
