71bcc4e8949bd79ac2295896e38a573b7bd23bdc
jxnhiro <kenny.jinhiro@gmail.com>
2022-12-04 16:33:05 +0700

    Added header and footer

--- resources/js/Pages/Pesanan.jsx
+++ resources/js/Pages/Pesanan.jsx
@@ -8,7 +8,17 @@ export default function Pesanan(){
             <div className="bg-blue-bg bg-cover bg-no-repeat">
                 <HeaderNoBg />
             </div>
-            
+            <div className="w-full h-max mt-8 mb-12 md:p-0 mini:p-3">
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-blue md:text-4xl sm:text-3xl mini:text-3xl font-bold mb-4 text-center">
+                        Pesanan Saya
+                    </h1>
+                    <h4 className="text-blue text-l font-bold text-center">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </h4>
+                </div>
+            </div>
+            <Footer/>
         </div>
     );
 }
