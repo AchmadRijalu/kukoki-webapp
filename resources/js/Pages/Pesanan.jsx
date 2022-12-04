3d12d8c0ff669a2fee5ca11d55311e68fa351faf
jxnhiro <kenny.jinhiro@gmail.com>
2022-12-04 17:37:08 +0700

    :+1: Formatting done

--- resources/js/Pages/Pesanan.jsx
+++ resources/js/Pages/Pesanan.jsx
@@ -9,14 +9,69 @@ export default function Pesanan(){
                 <HeaderNoBg />
             </div>
             <div className="w-full h-max mt-8 mb-12 md:p-0 mini:p-3">
-                <div className="flex flex-col justify-center items-center">
-                    <h1 className="text-blue md:text-4xl sm:text-3xl mini:text-3xl font-bold mb-4 text-center">
-                        Pesanan Saya
-                    </h1>
-                    <h4 className="text-blue text-l font-bold text-center">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    </h4>
+                <div className="flex flex-row justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-blue md:text-5xl sm:text-3xl mini:text-3xl font-bold mb-4 text-center">
+                            Pesanan Saya
+                        </h1>
+                        <h3 className="text-blue md:text-2xl sm:text-2xl mini:text-2xl text-l font-bold text-center mt-4">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </h3>
+                    </div>
                 </div>
+                {/* Sedang Berlangsung */}
+                <div className="flex flex-row mx-32 mt-12">
+                    <div className="flex flex-col h-full w-full">
+                        <div className="flex flex-row">
+                            <h2 className="text-blue md:text-3xl sm:text-3xl mini:text-3xl font-bold mb-8">
+                                Sedang Berlangsung
+                            </h2>
+                        </div>
+                        <div className="flex flex-row w-full">
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 border-none drop-shadow-md">
+                                <div>
+                                    <p>aasdddddddddddd</p>
+                                </div>
+                            </div>
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 border-none drop-shadow-md">
+                                <div>
+                                    <p>aasdddddddddddd</p>
+                                </div>
+                            </div>
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 border-none drop-shadow-md">
+                                <div>
+                                    <p>aasdddddddddddd</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Selesai */}
+                <div className="flex flex-row mx-32 mt-12">
+                    <div className="flex flex-col h-full w-full">
+                        <div className="flex flex-row">
+                            <h2 className="text-blue md:text-3xl sm:text-3xl mini:text-3xl font-bold mb-8">
+                                Selesai
+                            </h2>
+                        </div>
+                        <div className="flex flex-row w-full">
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 mt-6 border-none drop-shadow-md">
+                                <div><p>aasdddddddddddd</p></div>
+                                
+                            </div>
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 mt-6 border-none drop-shadow-md">
+                                <div><p>aasdddddddddddd</p></div>
+                                
+                            </div>
+                            <div className="lg:w-1/3 bg-white rounded-md p-6 mt-6 border-none drop-shadow-md">
+                                <div><p>aasdddddddddddd</p></div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
             <Footer/>
         </div>
